9b451a16f6772b580a416a9f808563f0eb189022
Dara Parvizi <dara0046@edu.easj.dk>
2019-03-19 18:51:31 +0100

    Add project files.

---
+++ TCPClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPClient
+{
+    class Program
+    {
+        static void Main(string[] args) { 
+
+
+        TcpClient clientSocket = new TcpClient("localhost", 6789);
+        Console.WriteLine("client started");
+
+
+        NetworkStream ns = clientSocket.GetStream();
+        StreamWriter sw = new StreamWriter(ns);
+        StreamReader sr = new StreamReader(ns);
+        sw.AutoFlush = true;
+
+        while (true)
+        {
+            Console.WriteLine(sr.ReadLine());
+            sw.WriteLine(Console.ReadLine());
+        }
+
+
+        Console.ReadLine();
+        ns.Close();
+            clientSocket.Close();
+        }
+    }
+}
